12096dfd8c4614eefa13b84bb0c14e2ac8d971bb
sdevarajan2006 <101810634+sdevarajan2006@users.noreply.github.com>
2023-03-15 09:05:03 -0500

    new files

--- individual_window_breakdown.py
+++ individual_window_breakdown.py
@@ -1,12 +1,4 @@
 import requests
-# post something to the web (post call)
-# get something from the web (get call)
-    # where to get it from (URL)
-    # Pass an identification key
-    # pass a parameter
-    # 
-
-
 API_URL = "https://api-inference.huggingface.co/models/distilbert-base-uncased-finetuned-sst-2-english"
 headers = {"Authorization": "Bearer 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"}
 
@@ -14,25 +6,28 @@ def query(payload):
 	response = requests.post(API_URL, headers=headers, json=payload)
 	return response.json()
 
-def run_sentiment_analysis(text):
-	number_of_sections = len(text)
-	positive_counter = 0
-	negative_counter = 0
+def individual_window_breakdown(text):
+	list_of_sentences_and_sentiments = []
 	for i in text:
 		output = query({"inputs" : i,
 		  })
 		if output[0][0]['label'] == 'POSITIVE':
-			positive_counter += output[0][0]['score']
-			negative_counter += output[0][1]['score']
+			this_window = {
+				"SENTENCES" : i,
+				"POSITIVE" : output[0][0]['score'],
+				"NEGATIVE" : output[0][1]['score']
+            }
 		else:
-			positive_counter += output[0][1]['score']
-			negative_counter += output[0][0]['score']
-	average_positive_score = positive_counter/number_of_sections
-	average_negative_score = negative_counter/number_of_sections
-	return("POSITIVE: " + str(average_positive_score) + "\n" + "NEGATIVE: " + str(average_negative_score))
-	
-	
+			this_window = {
+				"SENTENCES" : i,
+				"POSITIVE" : output[0][1]['score'],
+				"NEGATIVE" : output[0][0]['score']
+            }
+		list_of_sentences_and_sentiments.append(this_window)
+	return(list_of_sentences_and_sentiments)
+
 if __name__ == '__main__':
 	list_sample = ['Snakes are elongated, legless reptiles belonging to the suborder Serpentes. They are found in diverse habitats around the world, ranging from deserts to rainforests. Snakes have a flexible body and can move quickly, enabling them to catch prey and escape from predators.', ' They have a unique adaptation for killing prey - their jaws are hinged and can be opened wide, allowing them to swallow prey much larger than their head. Some species of snakes are venomous and their bite can be dangerous to humans, but many are harmless. Snakes play important roles in the ecosystems in which they live, serving as both predator and prey.', ' yqsssss']
-	ans = run_sentiment_analysis(list_sample)
+	ans = individual_window_breakdown(list_sample)
 	print(ans)
+	
